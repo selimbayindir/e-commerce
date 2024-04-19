d18423c792ec17f443c78225e2c50f4be4714da8
Selim BAYINDIR <byndr28@gmail.com>
2024-04-19 10:10:24 +0300

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Policy from './components/Policy';
+function App() {
+
+  return (
+    <div>
+      <Header/>
+    <h1>Hello</h1>
+    <Policy/>
+    <Footer/>
+    </div>
+  
+  )
+}
+
+export default App
