bc8ebb33be2abb5337a1c0315456cb118975d126
Selim BAYINDIR <byndr28@gmail.com>
2024-04-19 16:53:22 +0300

    caategories

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 
 import './App.css'
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Policy from './components/Policy';
+import { Categories } from './components/Layout/Categories/Categories';
+import Footer from './components/Layout/Footer/Footer';
+import Header from './components/Layout/Header/Header';
+import Policy from './components/Layout/Policy/Policy';
+import Slider from './components/Layout/Slider/Sliders';
 function App() {
 
   return (
     <div>
       <Header/>
-    <h1>Hello</h1>
+      <Slider/>
+      <Categories/>
     <Policy/>
     <Footer/>
     </div>
